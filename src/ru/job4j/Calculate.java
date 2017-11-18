03db1ecf9b3fdaff4d4d56f6e6fb34fbe60d6181
dmitrytishchenko <tdmitriu@mail.ru>
2017-11-18 22:03:58 +0300

    1.1 Создание Hello World

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+/**
+* Это первая моя программа
+*@version 1.01 18.11.2017
+*@author Dmitriy Tishchenko
+*/
+public class Calculate{
+public static void main(String[] args){
+	System.out.println("Hello world.");
+}
+}
