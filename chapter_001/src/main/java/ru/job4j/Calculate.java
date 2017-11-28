c492532469584e355219671f94f02583c167183c
dmitrytishchenko <tdmitriu@mail.ru>
2017-11-28 22:33:39 +0300

    2.3. Элементарный калькулятор.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,20 +1,20 @@
 package ru.job4j;
 /**
-*Это вторая моя программа
-*@version 1.02 19.11.2017
-*@author Dmitriy Tishchenko
-*/
+ *Это вторая моя программа
+ *@version 1.02 19.11.2017
+ *@author Dmitriy Tishchenko
+ */
 public class Calculate {
-public static void main(String[] args) {
-    System.out.println("Hello world.");
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
     }
- 
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,20 +7,20 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-* Это вторая моя программа
-*@version 1.02 19.11.2017
-*@author Dmitriy Tishchenko
-*/
+ * Test.
+ * Это вторая моя программа
+ *@version 1.02 19.11.2017
+ *@author Dmitriy Tishchenko
+ */
 public class CalculateTest {
-	/**
-	*Test echo.
-	*/	@Test
-	public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Dmitriy Tishchenko";
-    String expect = "Echo, echo, echo : Dmitriy Tishchenko"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    /**
+     *Test echo.
+     */	@Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Dmitriy Tishchenko";
+        String expect = "Echo, echo, echo : Dmitriy Tishchenko";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
