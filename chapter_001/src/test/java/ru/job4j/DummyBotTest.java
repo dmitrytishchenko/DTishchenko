22ca726b2ea598aa14d825357a1b68b28b64dded
dmitrytishchenko <tdmitriu@mail.ru>
2017-12-04 00:49:21 +0300

    3.1. Глупый бот.

---
+++ chapter_001/src/test/java/ru/job4j/DummyBotTest.java
@@ -0,0 +1,38 @@
+package ru.job4j;
+import org.junit.Test;
+import ru.job4j.condition.DummyBot;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ * Это мой третий тест
+ *@version 2.03 3.12.2017
+ *@author Dmitriy Tishchenko
+ */
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+    @Test
+    public void whenByuBot(){
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите меня другой вопрос.")
+        );
+    }
+}
