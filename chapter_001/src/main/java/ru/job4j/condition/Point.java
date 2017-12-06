82eb02827678535bd28ce6856671983ef2cdf996
dmitrytishchenko <tdmitriu@mail.ru>
2017-12-07 00:59:20 +0300

    2.4. Расстояние между точками в системе координат.

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -14,20 +14,9 @@ public class Point {
         this.y = y;
     }
     public double distanceTo(Point that){
-        //Точка А - это текущая точка. К ней мы обращаемся через оператор this.
-        Point a = this;
-        //Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной that.
-        //или для удобства мы создали новую переменную b и к ней присвоили переменную this.
-        Point b = that;
-        int x1 = a.x;
-        int y1 = a.y;
-        int x2 = b.x;
-        int y2 = b.y;
-        double result = Math.sqrt(
-                Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
-        );
-        return result;
-    }
+
+        return = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+             }
     public static void main(String[] args) {
         Point a = new Point(0, 1);
         Point b = new Point(2, 5);
