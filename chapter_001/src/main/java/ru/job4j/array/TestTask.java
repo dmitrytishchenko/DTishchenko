37c6c8f57758ca7370667b1007cb6083e3b46659
dmitrytishchenko <tdmitriu@mail.ru>
2018-02-25 19:57:51 +0300

    7.1 Проверка, что одно слово находится в другом слове

---
+++ chapter_001/src/main/java/ru/job4j/array/TestTask.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class TestTask {
+    private char[] data;
+    private char[] value;
+        public boolean conteins(String origin, String sub) {
+        boolean result = false;
+        int temp = 0;
+        char[] data = origin.toCharArray();
+        char[] value = sub.toCharArray();
+            for (int j = 0; j < origin.length(); j++) {
+                if (data[j] == value[temp]) {
+                    temp++;
+                    if  (temp == sub.length()){
+                        result = true;
+                        return result;
+                    }
+                }
+                else {
+                    temp = 0;
+                }
+            }return result;
+        }
+    }
+
+
+
+
+
+
+
+
