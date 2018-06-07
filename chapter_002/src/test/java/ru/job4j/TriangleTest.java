6693fc8ae0a8b7d7f2d67de7e75fd296bb8f2c70
dmitrytishchenko <tdmitiu@mail.ru>
2018-06-08 02:09:15 +0300

    4. Используя шаблон проектирования - стратегию [#786]

---
+++ chapter_002/src/test/java/ru/job4j/TriangleTest.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void whenDrawTriangle(){
+        Triangle triangle = new Triangle();
+        assertThat(triangle.draw(), is(new StringBuilder()
+        .append("  +  ")
+        .append(" ++ ")
+        .append(" +++ ")
+        .append("+++++")
+        .toString()));
+    }
+
+}
