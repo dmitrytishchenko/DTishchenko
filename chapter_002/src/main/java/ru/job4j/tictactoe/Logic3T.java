3a37e4e681c2760da7f9761bfb4fec5a4fb081cf
dmitrytishchenko <tdmitiu@mail.ru>
2018-08-23 22:50:37 +0300

    Крестики-нолики на JavaFX [#49777] version 4

--- chapter_002/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_002/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -24,14 +24,23 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, this.table.length - 2, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 2, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, -1);
+        boolean result = false;
+        for (int i = 1; i <= this.table.length; i++) {
+            for (int j = 1; j <= this.table.length; j++) {
+                if (this.fillBy(Figure3T::hasMarkX, i, j, i + 1, j)
+                        || this.fillBy(Figure3T::hasMarkX, i, j, i, j + 1)
+                    || this.fillBy(Figure3T::hasMarkX, i, j, i + 1, j + 1));
+            } result = true;
+
+//        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+//                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+//                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+//                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1)
+//                || this.fillBy(Figure3T::hasMarkX, this.table.length - 2, 0, 0, 1)
+//                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0)
+//                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 2, 1, 0)
+//                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, -1);
+        }return result;
     }
 
     public boolean isWinnerO() {
@@ -50,10 +59,8 @@ public class Logic3T {
             for (int j = 0; j < this.table.length; j++) {
                 if (!this.table[i][j].hasMarkX() && !this.table[i][j].hasMark0()) {
                     result = true;
-                } else {
-
-                }
-            }break;
+                } break;
+            }
         }
         return result;
     }
