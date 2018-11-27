0f18161dc6c2ce49d236cfd3179b317ad6891a59
dmitrytishchenko <tdmitiu@mail.ru>
2018-11-27 12:17:36 +0300

    CoffeeMashine version2

--- chapter_003/src/main/java/ru/job4j/search/CoffeeMashine.java
+++ chapter_003/src/main/java/ru/job4j/search/CoffeeMashine.java
@@ -1,9 +1,9 @@
 package ru.job4j.search;
 
-        import com.sun.deploy.util.ArrayUtil;
+import com.sun.deploy.util.ArrayUtil;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoffeeMashine {
     /*
