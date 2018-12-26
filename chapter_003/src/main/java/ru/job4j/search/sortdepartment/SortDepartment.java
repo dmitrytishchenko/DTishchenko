47a3927b9d2451b4445b2de319639fad1b711170
dmitrytishchenko <tdmitiu@mail.ru>
2018-12-26 21:44:23 +0300

    Отсортировать департаменты [#34352] version 5

--- chapter_003/src/main/java/ru/job4j/search/sortdepartment/SortDepartment.java
+++ chapter_003/src/main/java/ru/job4j/search/sortdepartment/SortDepartment.java
@@ -24,26 +24,52 @@ public class SortDepartment {
     }
 
     public String[] reverseSort(String[] depart) {
-        Set<String> descendingSet = new TreeSet<>(new Comparator<String>() {
+        List<String> list = new ArrayList<>(Arrays.asList(depart));
+        list.sort(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 int result = 0;
-                if (o1.length() > o2.length()) {
-                    result = 1;
-                } else if (o1.length() < o2.length()) {
-                    result = -1;
-                } else if (o1.length() == o2.length()) {
-                    int min = Math.min(o1.length(), o2.length());
-                    for (int i = 0; i < min; i++) {
-                        if (o1.charAt(i) != o2.charAt(i)) {
-                            result = o2.compareTo(o1);
-                            break;
-                        }
+                String[] mas1 = o1.split("\\\\");
+                String[] mas2 = o2.split("\\\\");
+                int minim = Math.min(mas1.length, mas2.length);
+                for (int i = 0; i < minim; i++) {
+                    if (!mas1[i].equals(mas2[i])) {
+                        result = mas2[i].compareTo(mas1[i]);
+                        break;
+                    }
+                }
+                if (result == 0) {
+                    if (mas1.length > mas2.length) {
+                        result = 1;
+                    } else {
+                        result = -1;
                     }
                 }
                 return result;
             }
         });
-        return descendingSet.toArray(new String[descendingSet.size()]);
+        return list.toArray(new String[list.size()]);
+//        Set<String> descendingSet = new TreeSet<>(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                int result = 0;
+//                if (o1.length() > o2.length()) {
+//                    result = 1;
+//                } else if (o1.length() < o2.length()) {
+//                    result = -1;
+//                } else if (o1.length() == o2.length()) {
+//                    int min = Math.min(o1.length(), o2.length());
+//                    for (int i = 0; i < min; i++) {
+//                        if (o1.charAt(i) != o2.charAt(i)) {
+//                            result = o2.compareTo(o1);
+//                            break;
+//                        }
+//                    }
+//                }
+//                return result;
+//            }
+//        });
+//        return descendingSet.toArray(new String[descendingSet.size()]);
+
     }
 }
