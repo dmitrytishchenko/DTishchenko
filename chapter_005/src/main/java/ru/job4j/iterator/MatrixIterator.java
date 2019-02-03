b7d0aa9aabdffd4b83f682bc82431803ecc2782c
dmitrytishchenko <tdmitiu@mail.ru>
2019-02-03 18:43:23 +0300

    5.1.1. Итератор для двухмерного массива int[][][#110141]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,53 @@
+package ru.job4j.iterator;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class MatrixIterator implements Iterator {
+    int[][] array;
+    int count = 0;
+    int caret = 0;
+
+    public MatrixIterator(int[][] array) {
+        this.array = array;
+        convert();
+    }
+    public int convert() {
+        int[] result = Arrays.stream(this.array).flatMapToInt(n -> Arrays.stream(n)).toArray();
+        count = result.length;
+        return count;
+    }
+    public int getNumber() {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j <array[i].length ; j++) {
+                if (index == caret) {
+                    return this.array[i][j];
+                } else {
+                    index++;
+                }
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return caret < count;
+    }
+
+    @Override
+    public Integer next() {
+        Integer result = null;
+        if (hasNext()) {
+            result = getNumber();
+            caret++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MatrixIterator matrixIterator = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+        System.out.println(matrixIterator.getNumber());
+    }
+}
