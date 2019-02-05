e7aea62633a118857464570c49dbc3738de4cae0
dmitrytishchenko <tdmitiu@mail.ru>
2019-02-05 19:24:24 +0300

    5.1.1. Итератор для двухмерного массива int[][][#110141] version 3

--- chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -1,47 +1,36 @@
 package ru.job4j.iterator;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 public class MatrixIterator implements Iterator {
     int[][] array;
-    int count = 0;
-    int caret = 0;
+    int str = 0;
+    int stlb = 0;
 
     public MatrixIterator(int[][] array) {
         this.array = array;
-        convert();
     }
-    public int convert() {
-        int[] result = Arrays.stream(this.array).flatMapToInt(n -> Arrays.stream(n)).toArray();
-        count = result.length;
-        return count;
-    }
-    public int getNumber() {
-        int index = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j <array[i].length ; j++) {
-                if (index == caret) {
-                    return this.array[i][j];
-                } else {
-                    index++;
-                }
-            }
+
+    public void getNumber() {
+        if (stlb < array[str].length - 1) {
+            stlb++;
+        } else {
+            str++;
+            stlb = 0;
         }
-        return 0;
     }
 
     @Override
     public boolean hasNext() {
-        return caret < count;
+        return str < array.length && stlb < array[str].length;
     }
 
     @Override
     public Integer next() {
         Integer result = null;
         if (hasNext()) {
-            result = getNumber();
-            caret++;
+            result = this.array[str][stlb];
+            getNumber();
         }
         return result;
     }
