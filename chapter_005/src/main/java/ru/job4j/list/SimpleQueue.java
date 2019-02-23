919ce2c9958c8c6bb427baa7bf608f578af5cc2a
dmitrytishchenko <tdmitiu@mail.ru>
2019-02-23 11:35:05 +0300

    5.3.3.1 Очередь на двух стеках[#110134] version 2

--- chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -1,9 +1,14 @@
 package ru.job4j.list;
 
 public class SimpleQueue<E> {
-    SimpleStack<E> stack = new SimpleStack<>();
+    SimpleStack<E> stack;
+
+    public SimpleQueue() {
+        this.stack = new SimpleStack<>();
+    }
+
     public E poll() {
-        return stack.list.removeFirst();
+        return stack.getstackDelFirst();
     }
     public void push(E value) {
         stack.push(value);
--- chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
@@ -1,12 +1,23 @@
 package ru.job4j.list;
 
 public class SimpleStack<E> {
-
     SimpleLinkedList<E> list = new SimpleLinkedList<>();
+    private E stackDelLast;
+    private E stackDelFirst;
+
+
     public E poll() {
         return list.removeLast();
     }
     public void push(E value) {
         list.add(value);
+    }
+
+    public E getstackDelLast() {
+        return list.removeLast();
+    }
+
+    public E getstackDelFirst() {
+        return list.removeFirst();
     }
 }
