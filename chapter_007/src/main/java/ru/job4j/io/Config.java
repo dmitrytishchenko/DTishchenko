62e7e886aef8918178f08d44d97b689d8fe4ee24
dmitrytishchenko <tdmitiu@mail.ru>
2019-08-13 12:44:39 +0300

    1. Проверить байтовый поток[#110169]

---
+++ chapter_007/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            String s = null;
+            String[] s1 = null;
+            while ((s = reader.readLine()) != null) {
+                if ((!s.isEmpty()) && (s.charAt(0) != '#')) {
+                    s1 = s.split("=");
+                    values.put(s1[0], s1[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+}
