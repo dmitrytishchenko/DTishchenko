fa1a546e75775262cbbe6b5b075e028ad582ab39
dmitrytishchenko <tdmitiu@mail.ru>
2019-09-19 21:38:03 +0300

    5. Создать программу 'Консольный чат'. [#110172] ver2

--- chapter_007/src/main/java/ru/job4j/io/chat/ConsoleChat.java
+++ chapter_007/src/main/java/ru/job4j/io/chat/ConsoleChat.java
@@ -2,9 +2,13 @@ package ru.job4j.io.chat;
 
 import java.io.*;
 import java.util.Random;
+import java.util.Scanner;
 
 @SuppressWarnings("Duplicates")
 public class ConsoleChat {
+    private final String stop = "стоп";
+    private final String finish = "закончить";
+    private final String resume = "продолжить";
 
     public void chat() {
         String s;
@@ -35,6 +39,7 @@ public class ConsoleChat {
         String s;
         boolean stopped = false;
         String[] answers = new String[]{"Здравствуйте", "Как Ваши дела", "Ваше обращение очень важно для нас", "Счастливого пути"};
+        int rand = new Random().nextInt(answers.length);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new FileWriter("Query.txt"))) {
             do {
@@ -42,17 +47,29 @@ public class ConsoleChat {
                 if (s.toLowerCase().equals("стоп")) {
                     stopped = true;
                 } else if (s.toLowerCase().equals("продолжить")) {
-                    stopped = false;
+                    try (Scanner console = new Scanner(System.in);
+                         BufferedWriter bwriter = new BufferedWriter(new FileWriter("Query.txt"))) {
+                        do {
+                            s = console.nextLine();
+                            if (stop.equals(s.toLowerCase())) {
+                                stopped = true;
+                            } else if (resume.equals(s.toLowerCase())) {
+                                stopped = false;
+                            }
+                            if (stopped) {
+                                bwriter.write(s);
+                            } else {
+
+                                writer.write(String.format("%s" + "-" + "%s", s, answers[rand]));
+                                System.out.println(answers[rand]);
+                            }
+                        }
+                        while (!s.toLowerCase().equals("закончить"));
+                        writer.write(String.format("%s-%s", s, answers[rand]));
+                        System.out.println(answers[rand]);
+                    }
                 }
-                if (stopped) {
-                    writer.write(s);
-                } else {
-                    int rand = new Random().nextInt(answers.length);
-                    writer.write(String.format("%s" + "-" + "%s", s, answers[rand]));
-                    System.out.println(answers[rand]);
-                }
-            }
-            while (!s.toLowerCase().equals("закончить"));
+            } while (finish.equals(s.toLowerCase()));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -63,3 +80,4 @@ public class ConsoleChat {
         consoleChat.chat();
     }
 }
+
