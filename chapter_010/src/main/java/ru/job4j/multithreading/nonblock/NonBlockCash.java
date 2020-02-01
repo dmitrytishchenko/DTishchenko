6a3d9f21112ef9b719611c7ea277decbcd541b7e
dmitrytishchenko <tdmitiu@mail.ru>
2020-02-01 15:44:02 +0300

    1. Неблокирующий кеш[#218125]ver3

--- chapter_010/src/main/java/ru/job4j/multithreading/nonblock/NonBlockCash.java
+++ chapter_010/src/main/java/ru/job4j/multithreading/nonblock/NonBlockCash.java
@@ -13,24 +13,31 @@ public class NonBlockCash {
         this.map.put(model.getId(), model);
     }
 
-    public void update(Base model) {
+    public synchronized void update(Base model) {
         this.map.computeIfPresent(model.getId(), (id, base) -> {
-            if (model.getVersion() != this.map.get(model.getId()).getVersion()) {
+            if (model.getVersion() != getVers(model)) {
                 throw new OptimisticException("Версия не совпадает!");
             }
-            while (base.getId() == model.getId()) {
+            if (base.getId() == model.getId()) {
                 base = model;
-                base.setVersion(model.getVersion() + 1);
+                increment(base);
             }
             return base;
         });
-
     }
 
     public void delete(Base model) {
         if (this.map.size() > 0) {
             this.map.remove(model.getId(), model);
         }
+    }
+
+    public synchronized int getVers(Base model) {
+        return this.map.get(model.getId()).getVersion();
+    }
+
+    public synchronized void increment(Base model) {
+        model.setVersion(model.getVersion() + 1);
     }
 }
 
