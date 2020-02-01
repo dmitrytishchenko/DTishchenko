e4f5487d8f7c2b43e0d2f9bc64969eaa8557e20b
dmitrytishchenko <tdmitiu@mail.ru>
2020-02-01 10:16:57 +0300

    1. Неблокирующий кеш[#218125]ver2

--- chapter_010/src/main/java/ru/job4j/multithreading/nonblock/NonBlockCash.java
+++ chapter_010/src/main/java/ru/job4j/multithreading/nonblock/NonBlockCash.java
@@ -14,11 +14,11 @@ public class NonBlockCash {
     }
 
     public void update(Base model) {
-        if (model.getVersion() != this.map.get(model.getId()).getVersion()) {
-            throw new OptimisticException("Версия не совпадает!");
-        }
         this.map.computeIfPresent(model.getId(), (id, base) -> {
-            if (base.getId() == model.getId()) {
+            if (model.getVersion() != this.map.get(model.getId()).getVersion()) {
+                throw new OptimisticException("Версия не совпадает!");
+            }
+            while (base.getId() == model.getId()) {
                 base = model;
                 base.setVersion(model.getVersion() + 1);
             }
@@ -32,22 +32,5 @@ public class NonBlockCash {
             this.map.remove(model.getId(), model);
         }
     }
-
-//    public static void main(String[] args) {
-//        NonBlockCash cash = new NonBlockCash();
-//        Base base1 = new Base();
-//        base1.setId(1);
-//        base1.setVersion(1);
-//        Base base2 = new Base();
-//        base2.setId(2);
-//        base2.setVersion(1);
-//        cash.add(base1);
-//        cash.add(base2);
-//        Base base3 = new Base();
-//        base3.setId(1);
-//        base3.setVersion(1);
-//        cash.update(base3);
-//        System.out.println(cash.map.get(1).getVersion());
-//    }
 }
 
