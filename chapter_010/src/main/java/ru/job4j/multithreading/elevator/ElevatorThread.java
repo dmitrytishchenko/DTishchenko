9b6cdab7036c5d9d8e6a02a389fdc851bfd9d188
dmitrytishchenko <tdmitiu@mail.ru>
2020-02-20 21:43:24 +0300

    Лифт [#34595]ver3

--- chapter_010/src/main/java/ru/job4j/multithreading/elevator/ElevatorThread.java
+++ chapter_010/src/main/java/ru/job4j/multithreading/elevator/ElevatorThread.java
@@ -1,58 +1,55 @@
 package ru.job4j.multithreading.elevator;
 
-import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class ElevatorThread implements Runnable {
+public class ElevatorThread extends Thread {
     private Args args;
-    private CountDownLatch cd;
     private int move = 0;
     private int request;
     private int level = 1;
-    private Scanner scanner = new Scanner(System.in);
+    private Lock lock = new ReentrantLock();
 
-    public ElevatorThread(CountDownLatch cd, int request, Args args) {
-        this.cd = cd;
-        this.request = request;
+    public ElevatorThread(Args args, int request) {
         this.args = args;
-        new Thread(this).start();
+        this.request = request;
     }
 
     @Override
     public void run() {
-        while (true) {
-            call();
-            if (this.level == 1) {
-                movement();
-                for (int i = 1; i < this.move; i++) {
-                    cd.countDown();
-                    movementElevator(i);
-                }
-            } else if (this.level < this.request) {
-                movement();
-                for (int i = this.level; i < this.request; i++) {
-                    cd.countDown();
-                    movementElevator(i);
+        lock.lock();
+        workElevator();
+    }
 
-                }
-                this.level = this.request;
-            } else {
-                movement();
-                for (int i = this.level; i > this.request; i--) {
-                    cd.countDown();
-                    movementElevator(i);
-                }
-                this.level = this.request;
+    public void workElevator() {
+        lock.lock();
+        if (this.level == 1) {
+            movement();
+            for (int i = 1; i < this.move; i++) {
+                movementElevator(i);
             }
-            System.out.println("Лифт прибыл на " + this.level + " этаж");
-            try {
-                System.out.println("Двери лифта открыты");
-                Thread.sleep(Integer.valueOf(this.args.getTimeOpenDoors()) * 1000);
-                System.out.println("Двери лифта закрыты");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        } else if (this.level < this.request) {
+            movement();
+            for (int i = this.level; i < this.request; i++) {
+                movementElevator(i);
             }
+            this.level = this.request;
+        } else {
+            movement();
+            for (int i = this.level; i > this.request; i--) {
+                movementElevator(i);
+            }
+            this.level = this.request;
         }
+        System.out.println("Лифт прибыл на " + this.level + " этаж");
+        try {
+            System.out.println("Двери лифта открыты");
+            Thread.sleep(Integer.valueOf(this.args.getTimeOpenDoors()) * 1000);
+            System.out.println("Двери лифта закрыты");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        lock.unlock();
     }
 
     public void movementElevator(int value) {
@@ -75,24 +72,5 @@ public class ElevatorThread implements Runnable {
             this.move = this.request - this.level;
         }
         return this.move;
-    }
-
-    public void call() {
-        System.out.println("Укажите где Вы находитесь : лифт или подъезд");
-        if (scanner.next().equals("лифт")) {
-            request = callFromElevator();
-        } else {
-            request = callFromTheEntrance();
-        }
-    }
-
-    public int callFromElevator() {
-        System.out.println("Вызов команды из лифта. Укажите этаж.");
-        return scanner.nextInt();
-    }
-
-    public int callFromTheEntrance() {
-        System.out.println("Вызов команды из подъезда. Укажите этаж.");
-        return scanner.nextInt();
     }
 }
--- chapter_010/src/main/java/ru/job4j/multithreading/elevator/Main.java
+++ chapter_010/src/main/java/ru/job4j/multithreading/elevator/Main.java
@@ -2,6 +2,20 @@ package ru.job4j.multithreading.elevator;
 
 public class Main {
     public static void main(String[] args) {
-        new Elevator().start(args);
+        Args args1 = new Args(args);
+        ElevatorThread el1 = new ElevatorThread( args1, 5);
+        ElevatorThread el2 = new ElevatorThread( args1, 7);
+        el1.start();
+        try {
+            el1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        el2.start();
+        try {
+            el2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
